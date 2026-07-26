b008a41abee55cf21939281a773f1b0657529795
wanghq-thu <iot_wanghuaqiang@163.com>
2026-07-26 23:06:29 +0800

    添加一些使用说明
    Signed-off-by: wanghq-thu <iot_wanghuaqiang@163.com>

--- read_ur3_rtde.py
+++ read_ur3_rtde.py
@@ -1,3 +1,17 @@
+"""
+通过 RTDE 读取 UR3 机械臂的六个关节角。
+
+依赖安装：
+    pip install ur_rtde
+
+运行方式：
+    python3 read_ur3_rtde.py
+    python3 read_ur3_rtde.py <robot_ip>
+
+默认连接地址为 192.168.1.10。程序读取并打印 10 次关节角，
+相邻两次读取间隔 0.1 秒，关节角单位为弧度（rad）。
+"""
+
 import argparse
 import time
 
@@ -9,8 +23,8 @@ def main() -> None:
         description="读取 UR3 的六个关节角"
     )
     parser.add_argument(
-        "robot_ip",
-        nargs="?",
+        "robot_ip",  # 没有--，是位置参数
+        nargs="?",  # ?意思是参数是可选的，最多可以有一个值
         default="192.168.1.10",
         help="UR3 控制器的 IP 地址（默认：192.168.1.10）",
     )
